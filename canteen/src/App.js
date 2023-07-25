ee0c54160d8885a55acfd7faf49a2ecc7e342350
Sakshi Thombre <84434398+Sakshy18@users.noreply.github.com>
2023-07-25 13:21:21 +0530

    added app.js

--- canteen/src/App.js
+++ canteen/src/App.js
@@ -1,17 +1,19 @@
-import  { Routes, Route } from "react-router-dom";
-// import Home from "./Components/Home";
-import Home from "../src//Components/Home"
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Viewreport from "./Components/Viewreport";
 import Team from "./Components/Team";
+import PrintBill from "./Components/PrintBill";
 import "./Assets/Styles/Home.css";
-import "./Assets/Styles/Home.css";
+import "./Assets/Styles/Team.css";
+import "./Assets/Styles/PrintBill.css";
 
-export default function App()
-{
-  return
-  (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/team" element={<Team />} />
-    </Routes>
+export default function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/viewreport" element={<Viewreport />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/printbill" element={<PrintBill />} />
+      </Routes>
   );
 }
